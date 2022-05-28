48c8dad7c3053ee85085f75cdc5750d68e684dc8
cewbost <bostrom.ce@gmail.com>
2022-05-28 14:49:46 +0300

    add openzeppelin Ownable

--- contracts/decrFeat.sol
+++ contracts/decrFeat.sol
@@ -1,23 +1,9 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.4.22 <0.9.0;
 
-abstract contract decrOwned {
-  address owner;
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-  constructor() {
-    owner = msg.sender;
-  }
-
-  modifier onlyOwner {
-    require(
-      msg.sender == owner,
-      "Unauthorized."
-    );
-    _;
-  }
-}
-
-contract decrFeat is decrOwned {
+contract decrFeat is Ownable {
 
   string featName;
   mapping(address => bool) public receivers;
