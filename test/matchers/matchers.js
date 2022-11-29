b1926c90ff121b7ee5ae64d84a6a210e1802618b
Erik Boström <bostrom.ce@gmail.com>
2022-11-29 23:12:17 +0200

    add beInstanceOf matcher

--- test/matchers/matchers.js
+++ test/matchers/matchers.js
@@ -2,10 +2,12 @@ const { expect } = require("./expect.js");
 const { equal } = require("./equal.js");
 const { matchFields } = require("./match_fields.js");
 const { beNumber } = require("./be_number.js");
+const { beInstanceOf } = require("./be_instance_of.js");
 
 module.exports = {
   expect:       expect,
   equal:        equal,
   matchFields:  matchFields,
   beNumber:     beNumber,
+  beInstanceOf:     beInstanceOf,
 }
