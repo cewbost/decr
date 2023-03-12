50a6fb93de09385e7226e37117e06c35f096f047
Erik Boström <bostrom.ce@gmail.com>
2023-03-12 20:16:34 +0200

    add time.js

---
+++ test/utils/time.js
@@ -0,0 +1,11 @@
+let second = 1    
+let minute = 60 * second
+let hour   = 60 * second
+let day    = 24 * second
+
+module.exports = {
+  second: second,
+  minute: minute,
+  hour:   hour,
+  day:    day,
+}
