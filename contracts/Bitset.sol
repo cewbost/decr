36d09603a911d4f3cb817281a38af6f1f9d15be5
Erik Boström <bostrom.ce@gmail.com>
2023-07-16 19:11:48 +0300

    fixup bitset

--- contracts/Bitset.sol
+++ contracts/Bitset.sol
@@ -1,39 +1,28 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.4.22 <0.9.0;
 
-struct bitset {
-  bytes data;
+function getBit(bytes storage bts, uint idx) view returns(bool) {
+  return (bts[idx/8] & toBit(idx % 8)) != 0;
 }
 
-library Bitset {
-
-  function get(bitset storage bts, uint idx) public view returns(bool) {
-    bytes storage bs = bts.data;
-    return (bs[idx/8] & toBit(idx % 8)) != 0;
+function setBit(bytes storage bts, uint idx) {
+  uint byte_idx = idx / 8;
+  bytes1 bit = toBit(idx % 8);
+  uint len = bts.length;
+  if (byte_idx < len) {
+    bts[byte_idx] = bts[byte_idx] | bit;
+  } else {
+    for (; len < byte_idx; len++) bts.push();
+    bts.push() = bit;
   }
+}
 
-  function set(bitset storage bts, uint idx) public {
-    bytes storage bs = bts.data;
-    uint byte_idx = idx / 8;
-    bytes1 bit = toBit(idx % 8);
-    uint len = bs.length;
-    if (byte_idx < len) {
-      bs[byte_idx] = bs[byte_idx] | bit;
-    } else {
-      bytes memory mbs = new bytes(byte_idx + 1 - len);
-      mbs[mbs.length - 1] = bit;
-      bts.data = bytes.concat(bs, mbs);
-    }
-  }
+function unsetBit(bytes storage bts, uint idx) {
+  uint byte_idx = idx / 8;
+  if (byte_idx >= bts.length) return;
+  bts[byte_idx] = bts[byte_idx] & ~toBit(idx % 8);
+}
 
-  function unset(bitset storage bts, uint idx) public {
-    bytes storage bs = bts.data;
-    uint byte_idx = idx / 8;
-    if (byte_idx >= bs.length) return;
-    bs[byte_idx] = bs[byte_idx] & ~toBit(idx % 8);
-  }
-
-  function toBit(uint bit_idx) internal pure returns(bytes1) {
-    return bytes1(uint8(1 << bit_idx));
-  }
+function toBit(uint bit_idx) pure returns(bytes1) {
+  return bytes1(uint8(1 << bit_idx));
 }
--- test/bitset/bitset.sol
+++ test/bitset/bitset.sol
@@ -4,54 +4,54 @@ pragma solidity >=0.4.22 <0.9.0;
 import "truffle/Assert.sol";
 import "../../contracts/Bitset.sol";
 
-using Bitset for bitset;
+using { getBit, setBit, unsetBit } for bytes;
 
 contract TestBitset {
 
-  bitset set;
+  bytes set;
 
   function testSet() external {
-    set.set(0);
-    set.set(16);
-    set.set(41);
-    set.set(43);
-    set.set(66);
-    set.set(68);
-    set.set(70);
-    Assert.equal(string(set.data), hex"01000100000A000054", "should set some bits");
+    set.setBit(0);
+    set.setBit(16);
+    set.setBit(41);
+    set.setBit(43);
+    set.setBit(66);
+    set.setBit(68);
+    set.setBit(70);
+    Assert.equal(string(set), hex"01000100000A000054", "should set some bits");
   }
 
   function testUnset() external {
     bytes memory data = new bytes(4);
     for (uint i = 0; i < data.length; i++) data[i] = 0x0f;
-    set.data = data;
-    set.unset(0);
-    set.unset(9);
-    set.unset(18);
-    set.unset(27);
-    Assert.equal(string(set.data), string(hex"0E0D0B07"), "should unset some bits");
+    set = data;
+    set.unsetBit(0);
+    set.unsetBit(9);
+    set.unsetBit(18);
+    set.unsetBit(27);
+    Assert.equal(string(set), string(hex"0E0D0B07"), "should unset some bits");
   }
 
   function testGet() external {
     bytes memory data = new bytes(2);
     data[0] = 0x12;
     data[1] = 0x34;
-    set.data = data;
-    Assert.isTrue(set.get(1), "should get true");
-    Assert.isTrue(set.get(4), "should get true");
-    Assert.isTrue(set.get(10), "should get true");
-    Assert.isTrue(set.get(12), "should get true");
-    Assert.isTrue(set.get(13), "should get true");
-    Assert.isFalse(set.get(0), "should get false");
-    Assert.isFalse(set.get(2), "should get false");
-    Assert.isFalse(set.get(3), "should get false");
-    Assert.isFalse(set.get(5), "should get false");
-    Assert.isFalse(set.get(6), "should get false");
-    Assert.isFalse(set.get(7), "should get false");
-    Assert.isFalse(set.get(8), "should get false");
-    Assert.isFalse(set.get(9), "should get false");
-    Assert.isFalse(set.get(11), "should get false");
-    Assert.isFalse(set.get(14), "should get false");
-    Assert.isFalse(set.get(15), "should get false");
+    set = data;
+    Assert.isTrue(set.getBit(1), "should get true");
+    Assert.isTrue(set.getBit(4), "should get true");
+    Assert.isTrue(set.getBit(10), "should get true");
+    Assert.isTrue(set.getBit(12), "should get true");
+    Assert.isTrue(set.getBit(13), "should get true");
+    Assert.isFalse(set.getBit(0), "should get false");
+    Assert.isFalse(set.getBit(2), "should get false");
+    Assert.isFalse(set.getBit(3), "should get false");
+    Assert.isFalse(set.getBit(5), "should get false");
+    Assert.isFalse(set.getBit(6), "should get false");
+    Assert.isFalse(set.getBit(7), "should get false");
+    Assert.isFalse(set.getBit(8), "should get false");
+    Assert.isFalse(set.getBit(9), "should get false");
+    Assert.isFalse(set.getBit(11), "should get false");
+    Assert.isFalse(set.getBit(14), "should get false");
+    Assert.isFalse(set.getBit(15), "should get false");
   }
 }
