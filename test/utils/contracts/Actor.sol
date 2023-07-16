06c3e975644248798f78e8e768a48f0d88f37b7a
Erik Boström <bostrom.ce@gmail.com>
2023-07-16 19:31:50 +0300

    purge the rest of old aspect

--- test/utils/contracts/Actor.sol
+++ test/utils/contracts/Actor.sol
@@ -3,8 +3,6 @@ pragma solidity >=0.4.22 <0.9.0;
 
 import "./MotionSenderCaller.sol";
 import "../../../contracts/MotionHandler.sol";
-import "../../../contracts/OldAspect.sol";
-import "../../../contracts/OldAspect2.sol";
 import "../../../contracts/Aspect.sol";
 
 contract Actor is MotionSenderCaller {
@@ -20,22 +18,5 @@ contract Actor is MotionSenderCaller {
   function callResolveSucceeds(MotionHandler handler, uint issue) public returns(bool) {
     (, uint128 issue_id) = callResolve(handler, issue);
     return issue_id != 0;
-  }
-
-  function callRequestAspect(
-    OldAspect aspect,
-    bytes32 dets,
-    bytes32 hash
-  ) public returns(address, uint) {
-    return aspect.requestAspect(dets, hash);
-  }
-
-  function callAspectRequest(
-    OldAspect2 aspect,
-    uint32     generation,
-    bytes20    details,
-    bytes32    content
-  ) public {
-    aspect.request(generation, details, content);
   }
 }
