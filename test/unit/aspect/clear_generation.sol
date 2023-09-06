880ba2af54634d13ecc025258338d6d90e0cb786
Erik Boström <bostrom.ce@gmail.com>
2023-09-06 22:09:47 +0300

    split clear generation unit tests into multiple files

--- test/unit/aspect/clear_generation.sol
+++ test/unit/aspect/clear_generation.sol
@@ -65,35 +65,4 @@ contract TestAspectClearGeneration is AspectTestBase {
       contains(stored, hashes[7]),
       "Cleared records should be removed from user.");
   }
-
-  function testClearGenerationOnlyOwner() external {
-    AspectTestActor actor = newActors(1)[0];
-    try actor.clearGeneration("gen 1") {
-      Assert.fail("Should revert.");
-    } catch Error(string memory what) {
-      Assert.equal("Only owner can perform this action.", what,
-        "Should revert with right message.");
-    }
-  }
-
-  function testClearGenerationMustExist() external {
-    AspectTestActor actor = newActors(1)[0];
-    setOwner(address(actor));
-    try actor.clearGeneration("gen 1") {
-      Assert.fail("Should revert.");
-    } catch Error(string memory what) {
-      Assert.equal("Generation does not exist.", what, "Should revert with right message.");
-    }
-  }
-
-  function testClearGenerationMustBeExpired() external {
-    AspectTestActor actor = newActors(1)[0];
-    addGeneration(block.timestamp, block.timestamp + 10, "gen 1");
-    setOwner(address(actor));
-    try actor.clearGeneration("gen 1") {
-      Assert.fail("Should revert.");
-    } catch Error(string memory what) {
-      Assert.equal("Generation must be expired.", what, "Should revert with right message.");
-    }
-  }
 }
