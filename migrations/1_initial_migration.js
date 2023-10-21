af7ff93e6c7a96a8d76977e0d43803b9df2da049
Erik Boström <bostrom.ce@gmail.com>
2023-10-21 18:04:52 +0300

    simplify migration

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -1,15 +1,12 @@
 const Aspect     = artifacts.require("Aspect")
 const AspectBare = artifacts.require("AspectBare")
-const Console = artifacts.require("Console")
+const Console    = artifacts.require("Console")
 
 const { asEthWord } = require("../utils/ethword.js")
 
 const config = require("../config.js")
 
 module.exports = function (deployer, network, accounts) {
-  if (network == "test") {
-    deployer.deploy(Console, asEthWord("test aspect console"))
-  } else {
-    deployer.deploy(Console, asEthWord(config.console_tag))
-  }
+  let console_name = network == "test"? asEthWord("test aspect console") : asEthWord(config.console_tag)
+  deployer.deploy(Console, console_name)
 };
