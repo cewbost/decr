3ddd7e67a5e9dfa9db11943e9783c07da62b631c
Erik Boström <bostrom.ce@gmail.com>
2023-10-21 17:54:18 +0300

    remove deployment of shared library

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -1,6 +1,5 @@
 const Aspect     = artifacts.require("Aspect")
 const AspectBare = artifacts.require("AspectBare")
-const shared     = artifacts.require("shared")
 const Console = artifacts.require("Console")
 
 const { asEthWord } = require("../utils/ethword.js")
@@ -8,11 +7,7 @@ const { asEthWord } = require("../utils/ethword.js")
 const config = require("../config.js")
 
 module.exports = function (deployer, network, accounts) {
-  deployer.deploy(shared)
-  deployer.link(shared, Aspect)
-  deployer.link(shared, Console)
   if (network == "test") {
-    deployer.link(shared, AspectBare)
     deployer.deploy(Console, asEthWord("test aspect console"))
   } else {
     deployer.deploy(Console, asEthWord(config.console_tag))
